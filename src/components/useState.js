4435dca464f6d0cda25a08c9f935cbb6b6b6abbf
LAI CHEE HOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2024-01-24 22:38:04 +0800

    useState slice

--- src/components/useState.js
+++ src/components/useState.js
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useReducer, useState, memo } from "react";
 import "../App.css";
 import {
   Button,
@@ -9,16 +9,109 @@ import {
   FormHelperText,
 } from "@mui/material";
 
-const ACTION = {
-  INCREMENT_1: "increment_1",
-  DECREMENT_1: "decrement_1",
-  INCREMENT_2: "increment_2",
-  DECREMENT_2: "decrement_2",
-  INCREMENT_3: "increment_3",
-  DECREMENT_3: "decrement_3",
-  INPUT_TEXT: "input_text",
+
+/*** COMPONENTS *************************************************************************************************/
+const Counter1 = memo(
+  (prop) => {
+    function handleIncrementCounter1() {
+      prop.slice.add1();
+    }
+    function handleDecrementCounter1() {
+      prop.slice.minus1();
+    }
+    return (
+      <div className="counter">
+        <Button variant="text" onClick={handleDecrementCounter1}>
+          <div style={{ fontSize: "2rem" }}>-</div>
+        </Button>
+        <Chip sx={{ fontSize: "2rem" }} label={prop.state.counter1} />
+        <Button variant="text" onClick={handleIncrementCounter1}>
+          <div style={{ fontSize: "2rem" }}>+</div>
+        </Button>
+      </div>
+    );
+  },
+  (prevProps, nextProps) => {
+    return prevProps.state === nextProps.state;
+  }
+);
+
+const Counter2 = (prop) => {
+  console.log("Counter2");
+  function handleIncrementCounter2() {
+    prop.slice.add2();
+    // run({ type: ACTION.INCREMENT_2 });
+  }
+  function handleDecrementCounter2() {
+    prop.slice.minus2();
+    // run({ type: ACTION.DECREMENT_2 });
+  }
+  return (
+    <div className="counter">
+      <Button variant="text" onClick={handleDecrementCounter2}>
+        <div style={{ fontSize: "2rem" }}>-</div>
+      </Button>
+      <Chip sx={{ fontSize: "2rem" }} label={prop.state.counter2} />
+      <Button variant="text" onClick={handleIncrementCounter2}>
+        <div style={{ fontSize: "2rem" }}>+</div>
+      </Button>
+    </div>
+  );
 };
 
+const Counter3 = (prop) => {
+  console.log("Counter3");
+  function handleIncrementCounter3() {
+    prop.slice.add3();
+  }
+  function handleDecrementCounter3() {
+    prop.slice.minus3();
+  }
+  return (
+    <div className="counter">
+      <Button variant="text" onClick={handleDecrementCounter3}>
+        <div style={{ fontSize: "2rem" }}>-</div>
+      </Button>
+      <Chip sx={{ fontSize: "2rem" }} label={prop.state.counter3} />
+      <Button variant="text" onClick={handleIncrementCounter3}>
+        <div style={{ fontSize: "2rem" }}>+</div>
+      </Button>
+    </div>
+  );
+};
+
+const TextInput = memo(
+  (prop) => {
+    const handleInputOnChange = (event) => {
+      const inputText = event.target.value;
+      // prop.run({ type: ACTION.INPUT_TEXT, payload: inputText });
+      prop.slice.inputText(inputText);
+    };
+    return (
+      <FormControl variant="standard">
+        <InputLabel htmlFor="component-helper">Name</InputLabel>
+        <Input
+          id="component-helper"
+          defaultValue={prop.state.text}
+          aria-describedby="component-helper-text"
+          onChange={handleInputOnChange}
+        />
+        <FormHelperText id="component-helper-text">
+          {prop.state.text}
+        </FormHelperText>
+      </FormControl>
+    );
+  },
+  (prevProps, nextProps) => {
+    return prevProps.state === nextProps.state;
+  }
+);
+
+
+
+
+
+/*** PAGE *************************************************************************************************/
 function UseState() {
   const initState = {
     counter1: 0,
@@ -27,159 +120,52 @@ function UseState() {
     text: "",
   };
   const [state, setState] = useState(initState);
-  const run = (action) => {
-    switch (action.type) {
-      case ACTION.INCREMENT_1: {
-        setState({
-          ...state,
-          counter1: state.counter1 + 1,
-          counter2: state.counter2 + 1,
-          counter3: state.counter3 + 1,
-        });
-        break;
-      }
-      case ACTION.DECREMENT_1: {
-        setState({
-          ...state,
-          counter1: state.counter1 - 1,
-          counter2: state.counter2 - 1,
-          counter3: state.counter3 - 1,
-        });
-        break;
-      }
-      case ACTION.INCREMENT_2: {
-        setState({
-          ...state,
-          counter2: state.counter2 + 1,
-          counter3: state.counter3 + 1,
-        });
-        break;
-      }
-      case ACTION.DECREMENT_2: {
-        setState({
-          ...state,
-          counter2: state.counter2 - 1,
-          counter3: state.counter3 - 1,
-        });
-        break;
-      }
-      case ACTION.INCREMENT_3: {
-        setState({
-          ...state,
-          counter3: state.counter3 + 1,
-        });
-        break;
-      }
-      case ACTION.DECREMENT_3: {
-        setState({
-          ...state,
-          counter3: state.counter3 - 1,
-        });
-        break;
-      }
-      case ACTION.INPUT_TEXT: {
-        setState({
-          ...state,
-          text: action.payload,
-        });
-        break;
-      }
-      default:
-        break;
-    }
-  };
 
-  const Counter1 = () => {
-    function handleIncrementCounter1() {
-        run({ type: ACTION.INCREMENT_1 });
-    }
-    function handleDecrementCounter1() {
-        run({ type: ACTION.DECREMENT_1 });
-    }
-    return (
-      <div className="counter">
-        <Button variant="text" onClick={handleDecrementCounter1}>
-          <div style={{ fontSize: "2rem" }}>-</div>
-        </Button>
-        <Chip sx={{ fontSize: "2rem" }} label={state.counter1} />
-        <Button variant="text" onClick={handleIncrementCounter1}>
-          <div style={{ fontSize: "2rem" }}>+</div>
-        </Button>
-      </div>
-    );
-  };
-
-  const Counter2 = () => {
-    function handleIncrementCounter2() {
-        run({ type: ACTION.INCREMENT_2 });
-    }
-    function handleDecrementCounter2() {
-        run({ type: ACTION.DECREMENT_2 });
-    }
-    return (
-      <div className="counter">
-        <Button variant="text" onClick={handleDecrementCounter2}>
-          <div style={{ fontSize: "2rem" }}>-</div>
-        </Button>
-        <Chip sx={{ fontSize: "2rem" }} label={state.counter2} />
-        <Button variant="text" onClick={handleIncrementCounter2}>
-          <div style={{ fontSize: "2rem" }}>+</div>
-        </Button>
-      </div>
-    );
-  };
-
-  const Counter3 = () => {
-    function handleIncrementCounter3() {
-        run({ type: ACTION.INCREMENT_3 });
-    }
-    function handleDecrementCounter3() {
-        run({ type: ACTION.DECREMENT_3 });
-    }
-    return (
-      <div className="counter">
-        <Button variant="text" onClick={handleDecrementCounter3}>
-          <div style={{ fontSize: "2rem" }}>-</div>
-        </Button>
-        <Chip sx={{ fontSize: "2rem" }} label={state.counter3} />
-        <Button variant="text" onClick={handleIncrementCounter3}>
-          <div style={{ fontSize: "2rem" }}>+</div>
-        </Button>
-      </div>
-    );
-  };
-
-  const TextInput = () => { 
-    const [a, setA] = useState('');
-    const handleInputOnChange = (event) => {
-        const inputText = event.target.value;
-        // run({type: ACTION.INPUT_TEXT, payload: inputText})
-        setA(inputText);
-    }
-    return (
-      <FormControl variant="standard">
-        <InputLabel htmlFor="component-helper">Name</InputLabel>
-        <Input
-          id="component-helper"
-          defaultValue={a}
-          aria-describedby="component-helper-text"
-          onChange={handleInputOnChange}
-        />
-        <FormHelperText id="component-helper-text">
-          {a}
-        </FormHelperText>
-      </FormControl>
-    );
+  const slice = {
+    add1: () => {
+      state.counter1 += 1;
+      state.counter2 += 1;
+      state.counter3 += 1;
+      setState({ ...state });
+    },
+    minus1: () => {
+      state.counter1 -= 1;
+      state.counter2 -= 1;
+      state.counter3 -= 1;
+      setState({ ...state });
+    },
+    add2: () => {
+      state.counter2 += 1;
+      state.counter3 += 1;
+      setState({ ...state });
+    },
+    minus2: () => {
+      state.counter2 -= 1;
+      state.counter3 -= 1;
+      setState({ ...state });
+    },
+    add3: () => {
+      state.counter3 += 1;
+      setState({ ...state });
+    },
+    minus3: () => {
+      state.counter3 -= 1;
+      setState({ ...state });
+    },
+    inputText: (value) => {
+      state.text = value;
+      setState({ ...state });
+    },
   };
 
   return (
     <div>
       <h1>useState</h1>
-      <Counter1 />
-      <Counter2 />
-      <Counter3 />
+      <Counter1 slice={slice} state={state} />
+      <Counter2 slice={slice} state={state} />
+      <Counter3 slice={slice} state={state} />
       <div style={{ marginTop: "50px" }}>
-        <TextInput />
+        <TextInput slice={slice} state={state} />
       </div>
     </div>
   );
