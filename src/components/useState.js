799f29b312164c03a9bb659df0eb5b6d2e2d4d60
LAI CHEE HOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2024-01-25 11:21:05 +0800

    improve useState

--- src/components/useState.js
+++ src/components/useState.js
@@ -1,4 +1,4 @@
-import { useReducer, useState, memo } from "react";
+import { useState, memo } from "react";
 import "../App.css";
 import {
   Button,
@@ -8,6 +8,72 @@ import {
   Input,
   FormHelperText,
 } from "@mui/material";
+
+
+/*** PAGE *************************************************************************************************/
+function UseState() {
+  const initState = {
+    counter1: 0,
+    counter2: 0,
+    counter3: 0,
+    text: "",
+  };
+  const [state, setState] = useState(initState);
+  const slice = setStateTool(setState, state, {
+    add1: () => {
+      state.counter1 += 1;
+      state.counter2 += 1;
+      state.counter3 += 1;
+    },
+    minus1: () => {
+      state.counter1 -= 1;
+      state.counter2 -= 1;
+      state.counter3 -= 1;
+    },
+    add2: () => {
+      state.counter2 += 1;
+      state.counter3 += 1;
+    },
+    minus2: () => {
+      state.counter2 -= 1;
+      state.counter3 -= 1;
+    },
+    add3: () => {
+      state.counter3 += 1;
+    },
+    minus3: () => {
+      state.counter3 -= 1;
+    },
+    inputText: (value) => {
+      state.text = value;
+    },
+    resetState: () => {
+      setState(initState);
+    },
+  });
+
+
+  return (
+    <div>
+      <h1>useState</h1>
+      <Counter1 slice={slice} state={state} />
+      <Counter2 slice={slice} state={state} />
+      <Counter3 slice={slice} state={state} />
+      <div style={{ marginTop: "50px" }}>
+        <TextInput slice={slice} state={state} />
+      </div>
+      <ResetButton slice={slice} />
+    </div>
+  );
+}
+
+export default UseState;
+/****************************************************************************************************/
+
+
+
+
+
 
 
 /*** COMPONENTS *************************************************************************************************/
@@ -36,22 +102,20 @@ const Counter1 = memo(
   }
 );
 
-const Counter2 = (prop) => {
-  console.log("Counter2");
+const Counter2 = ({slice, state}) => {
   function handleIncrementCounter2() {
-    prop.slice.add2();
-    // run({ type: ACTION.INCREMENT_2 });
+    slice.add2();
   }
   function handleDecrementCounter2() {
-    prop.slice.minus2();
-    // run({ type: ACTION.DECREMENT_2 });
+    slice.minus2();
   }
+
   return (
     <div className="counter">
       <Button variant="text" onClick={handleDecrementCounter2}>
         <div style={{ fontSize: "2rem" }}>-</div>
       </Button>
-      <Chip sx={{ fontSize: "2rem" }} label={prop.state.counter2} />
+      <Chip sx={{ fontSize: "2rem" }} label={state.counter2} />
       <Button variant="text" onClick={handleIncrementCounter2}>
         <div style={{ fontSize: "2rem" }}>+</div>
       </Button>
@@ -59,20 +123,19 @@ const Counter2 = (prop) => {
   );
 };
 
-const Counter3 = (prop) => {
-  console.log("Counter3");
+const Counter3 = ({slice, state}) => {
   function handleIncrementCounter3() {
-    prop.slice.add3();
+    slice.add3();
   }
   function handleDecrementCounter3() {
-    prop.slice.minus3();
+    slice.minus3();
   }
   return (
     <div className="counter">
       <Button variant="text" onClick={handleDecrementCounter3}>
         <div style={{ fontSize: "2rem" }}>-</div>
       </Button>
-      <Chip sx={{ fontSize: "2rem" }} label={prop.state.counter3} />
+      <Chip sx={{ fontSize: "2rem" }} label={state.counter3} />
       <Button variant="text" onClick={handleIncrementCounter3}>
         <div style={{ fontSize: "2rem" }}>+</div>
       </Button>
@@ -107,68 +170,42 @@ const TextInput = memo(
   }
 );
 
-
-
-
-
-/*** PAGE *************************************************************************************************/
-function UseState() {
-  const initState = {
-    counter1: 0,
-    counter2: 0,
-    counter3: 0,
-    text: "",
-  };
-  const [state, setState] = useState(initState);
-
-  const slice = {
-    add1: () => {
-      state.counter1 += 1;
-      state.counter2 += 1;
-      state.counter3 += 1;
-      setState({ ...state });
-    },
-    minus1: () => {
-      state.counter1 -= 1;
-      state.counter2 -= 1;
-      state.counter3 -= 1;
-      setState({ ...state });
-    },
-    add2: () => {
-      state.counter2 += 1;
-      state.counter3 += 1;
-      setState({ ...state });
-    },
-    minus2: () => {
-      state.counter2 -= 1;
-      state.counter3 -= 1;
-      setState({ ...state });
-    },
-    add3: () => {
-      state.counter3 += 1;
-      setState({ ...state });
-    },
-    minus3: () => {
-      state.counter3 -= 1;
-      setState({ ...state });
-    },
-    inputText: (value) => {
-      state.text = value;
-      setState({ ...state });
-    },
+const ResetButton = ({slice, state}) => {
+  const resetState = () => {
+    slice.resetState();
   };
 
   return (
-    <div>
-      <h1>useState</h1>
-      <Counter1 slice={slice} state={state} />
-      <Counter2 slice={slice} state={state} />
-      <Counter3 slice={slice} state={state} />
-      <div style={{ marginTop: "50px" }}>
-        <TextInput slice={slice} state={state} />
-      </div>
-    </div>
+    <Button
+      variant="contained"
+      style={{ marginTop: "50px" }}
+      onClick={resetState}
+    >
+      Reset
+    </Button>
   );
-}
+};
+/****************************************************************************************************/
 
-export default UseState;
+
+
+
+
+
+
+/*** TOOLS *************************************************************************************************/
+const setStateTool = (setState, state, actions) => {
+  const wrappedActions = {};
+
+  for (const key in actions) {
+    wrappedActions[key] = (...args) => {
+      actions[key](...args);
+      if (key !== "resetState") {
+        setState({ ...state });
+      }
+    };
+  }
+
+  return wrappedActions;
+};
+/****************************************************************************************************/
